58a8ff12e05e7e35a841f500c41e0592d4d67f1d
SiimIlves <65825538+SiimIlves@users.noreply.github.com>
2021-11-17 12:19:03 +0200

    fiksed

---
+++ js.js
@@ -0,0 +1,26 @@
+const btn = document.getElementById("btn");
+const resultContainer = document.getElementById("results");
+function supamarobrodas(bub) {
+    var nick = document.getElementById('input').value;
+$.ajax({
+    url: "https://open.faceit.com/data/v4/players?nickname=" + nick,
+    dataType: 'json',
+    beforeSend: function(xhr) {
+         xhr.setRequestHeader("Authorization", "Bearer ad088ca9-18f2-4c4a-9196-44525b88e2cb")
+    }, success: function(data){
+        return data.json;
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+        if(xhr.status==404) {
+            alert('User not found!')
+                }
+    }
+}).then(({ nickname, player_id }) => {
+resultContainer.innerHTML = `
+PlayerName: ${nickname}
+PlayerID: ${player_id}`;
+}) 
+};
+btn.addEventListener("click", (event) => {
+    supamarobrodas();
+});
