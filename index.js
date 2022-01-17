f856c55ecc11cee5f0b39405dfe3eeaa375acc19
SiimIlves <65825538+SiimIlves@users.noreply.github.com>
2022-01-17 04:24:54 +0200

    No longer calls data from one specific FACEIT profile

--- index.js
+++ index.js
@@ -20,16 +20,14 @@ function supamarobrodas(bub) {
         alert("User not found!");
       }
     },
-  }).then(({ nickname, player_id, games }) => {
-    resultContainer.innerHTML = `
-Nickname: ${nickname} ||
+  }).then(({ avatar, nickname, player_id, games }) => {
+    resultContainer.innerHTML = ` <img style="margin-top: 25px; display:block; margin-left: auto; margin-right: auto; width: 200px; height: 200px" src=${avatar}>
+    Nickname: ${nickname} ||
 Elo: ${games.csgo.faceit_elo}`;
-    var id = player_id;
-  });
-
+var id = player_id;
+  
   $.ajax({
-    url:
-      "https://open.faceit.com/data/v4/players/ebe5cc0f-05cd-4f06-b56b-d2539fcf0d62/stats/csgo",
+    url: "https://open.faceit.com/data/v4/players/" + id + "/stats/csgo",
     dataType: "json",
     beforeSend: function (xhr) {
       xhr.setRequestHeader(
@@ -53,13 +51,14 @@ Lifetime: <br />
 <pre> Matches: ${lifetime.Matches} </pre>
 <pre> Winrate: ${lifetime["Win Rate %"]}% </pre>
 <br />
+<img src=${segments[0].img_regular}><br>
 Map: ${segments[0].label}
 <pre> Winrate: ${segments[0].stats["Win Rate %"]}% </pre>
 <pre> AVG K/D Ratio: ${segments[0].stats["Average K/D Ratio"]} </pre>
 <pre> AVG K/R Ratio: ${segments[0].stats["Average K/R Ratio"]} </pre>
 `;
   });
-}
+})};
 btn.addEventListener("click", (event) => {
   supamarobrodas();
 });
