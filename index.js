a8dfb1692ab1662eb661c3d52687f2fbda31b6d1
AndrewTheArtist <a.sutt@khk.ee>
2022-01-29 17:21:18 +0200

    Cut data into three segments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById("btn");
 const resultContainer = document.getElementById("results");
 const resultContainer2 = document.getElementById("results2");
+const resultContainer3 = document.getElementById("results3");
+
 
 function supamarobrodas(bub) {
   var nick = document.getElementById("input").value;
@@ -53,11 +55,36 @@ Lifetime: <br />
 <p> Matches: ${lifetime.Matches} </p>
 <pre> Winrate: ${lifetime["Win Rate %"]}% </pre>
 <br />
-<img  src=${segments[0].img_regular}><br>
-Map: ${segments[0].label}
-<pre> Winrate: ${segments[0].stats["Win Rate %"]}% </pre>
-<pre> AVG K/D Ratio: ${segments[0].stats["Average K/D Ratio"]} </pre>
-<pre> AVG K/R Ratio: ${segments[0].stats["Average K/R Ratio"]} </pre>
+
+`;
+  });
+
+
+  $.ajax({
+    url: "https://open.faceit.com/data/v4/players/" + id + "/stats/csgo",
+    dataType: "json",
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader(
+        "Authorization",
+        "Bearer ad088ca9-18f2-4c4a-9196-44525b88e2cb"
+      );
+    },
+    success: function (data) {
+      return data.json;
+    },
+    error: function (xhr, ajaxOptions, thrownError) {
+      if (xhr.status == 404) {
+        alert("Not found");
+      }
+    },
+  }).then(({ lifetime, segments }) => {
+    resultContainer3.innerHTML = `
+
+    <img  src=${segments[0].img_regular}><br>
+    Map: ${segments[0].label}
+    <pre> Winrate: ${segments[0].stats["Win Rate %"]}% </pre>
+    <pre> AVG K/D Ratio: ${segments[0].stats["Average K/D Ratio"]} </pre>
+    <pre> AVG K/R Ratio: ${segments[0].stats["Average K/R Ratio"]} </pre>
 
 `;
   });
@@ -71,6 +98,7 @@ btn.addEventListener("click", (e) => {
     document.getElementById('faceit-title').classList.add('faceit-title-after');
     document.getElementById('input').classList.add('search-after');
     document.getElementById('btn').classList.add('nupp');
+    document.getElementById('pepe').classList.add('pepsi');
 
     supamarobrodas();
 
